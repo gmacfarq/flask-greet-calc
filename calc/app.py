a117871b9dfd385587fcb9285334d589f9e31107
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-04-19 12:14:18 -0700

    welcome n calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,15 @@
-# Put your app in here.
+import operations
+from flask import Flask, request
+app = Flask(__name__)
+
+
+
+@app.get('/add')
+def add():
+    """add two numbers together"""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    html = f"<html><body><h1>{operations.add(a,b)}</h1></body></html>"
+    return html
---
+++ greet/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.get('/welcome')
+def say_welcome():
+    """Return a "Welcome" greeting"""
+
+    html = "<html><body><h1>welcome</h1></body></html>"
+    return html
+
+@app.get('/welcome/home')
+def say_welcome_home():
+    """Return a "Welcome home" greeting"""
+
+    html = "<html><body><h1>welcome home</h1></body></html>"
+    return html
+
+@app.get('/welcome/back')
+def say_welcome_back():
+    """Return a "Welcome back" greeting"""
+
+    html = "<html><body><h1>welcome back</h1></body></html>"
+    return html
+
